38550eeecb9737bf8dd86f83da62fbe2981b2b11
Bartosz Ostafin <ostafinbartek@wp.pl>
2020-01-13 20:33:31 +0100

    Spelling, req.txt

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # 2busy2homework 🙊
 
 **2busy2homework** is a tool that allows sending POST request to code.org for automatic excercise completion. 
-Using selenium it's capable of sending Form Data with *random* generated values fo attempts, completion time and lines of code.  
+Using selenium it's capable of sending Form Data with *random* generated values of attempts, completion time and lines of code.  
 
 Installation
 ------------
--- main.py
+++ main.py
@@ -23,7 +23,7 @@ class codeBOT:
         self.driver.find_element_by_xpath('//*[@id="signin-button"]')\
             .click()
 
-    def genFetch(self,iterator,ida,before,after,appName):
+    def genFetch(self,iterator,id,before,after,appName):
         FirstHalf = 'fetch("https://studio.code.org/milestone/{id}/{before}/{after}"'
         Middle = ', {"credentials":"include","headers":{"accept":"application/json, text/javascript, */*; q=0.01","accept-language":"pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7","content-type":"application/x-www-form-urlencoded","sec-fetch-mode":"cors","sec-fetch-site":"same-origin","x-newrelic-id":"UQYGVVBQGwAHXFZRAQU=","x-requested-with":"XMLHttpRequest"},"referrer":"https://studio.code.org/s/course3/stage/21/puzzle/1","referrerPolicy":"no-referrer-when-downgrade","body":"program=%3Cxml%3E%3Cblock%20type%3D%22when_run%22%20deletable%3D%22false%22%20movable%3D%22false%22%3E%3Cnext%3E%3Cblock%20type%3D%22controls_repeat%22%20deletable%3D%22false%22%20editable%3D%22false%22%3E%3Ctitle%20name%3D%22TIMES%22%3E4%3C%2Ftitle%3E%3Cstatement%20name%3D%22DO%22%3E%3Cblock%20type%3D%22draw_move_by_constant%22%3E%3Ctitle%20name%3D%22DIR%22%3EmoveForward%3C%2Ftitle%3E%3Ctitle%20name%3D%22VALUE%22%3E20%3C%2Ftitle%3E%3Cnext%3E%3Cblock%20type%3D%22draw_turn_by_constant%22%3E%3Ctitle%20name%3D%22DIR%22%3EturnRight%3C%2Ftitle%3E%3Ctitle%20name%3D%22VALUE%22%3E90%3C%2Ftitle%3E%3C%2Fblock%3E%3C%2Fnext%3E%3C%2Fblock%3E%3C%2Fstatement%3E%3C%2Fblock%3E%3C%2Fnext%3E%3C%2Fblock%3E%3C%2Fxml%3E&'
         LastHalf = 'app={appName}&level=custom&result=true&testResult=100&time={time}&lines={lines}&save_to_gallery=false&attempt={attempts}","method":"POST","mode":"cors"'
